70801897ddbc757f03bc82817f8dc5051f7ad7dd
Myron <dubtwee@gmail.com>
2019-12-12 10:19:44 -0600

    updating with lecture code for implementing material-ui components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// MATERIAL-UI
+// import Button from '@material-ui/core/Button';
+// import Grid from '@material-ui/core/Grid';
+// import Container from '@material-ui/core/Container';
+
+import {
+  Button,
+  Grid,
+  Container
+} from '@material-ui/core';
+
+// THEMEING
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import {
+  teal,
+  cyan,
+  red,
+} from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: cyan,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  }
+})
+
+class App extends Component {
+
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <h1>STYLE ME</h1>
+          </header>
+
+          <Container>
+            <Grid container spacing={5}>
+              <Grid item xs={12}>
+                <h2>Cell 1</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempor convallis condimentum. Donec ullamcorper mauris facilisis ante efficitur commodo. Vivamus congue blandit nulla et venenatis. Suspendisse nec scelerisque ipsum. In.</p>
+              </Grid>
+              <Grid item xs={6}>
+                <h2>Cell 2</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempor convallis condimentum. Donec ullamcorper mauris facilisis ante efficitur commodo. Vivamus congue blandit nulla et venenatis. Suspendisse nec scelerisque ipsum. In.</p>
+              </Grid>
+              <Grid item xs={6}>
+                <h2>Cell 3</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempor convallis condimentum. Donec ullamcorper mauris facilisis ante efficitur commodo. Vivamus congue blandit nulla et venenatis. Suspendisse nec scelerisque ipsum. In.</p>
+              </Grid>
+            </Grid>
+          </Container>
+
+          <Button variant="contained" color="primary">ME BUTTON</Button>
+          <Button variant="contained" color="secondary">ME BUTTON</Button>
+          <Button variant="contained" color="default">ME BUTTON</Button>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
